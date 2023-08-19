Added Linear, Lenet5, updated progress bar for result computation, fixed --recalculate-results argument.

code/models/lenet5.py
@@ -6,7 +6,7 @@ from models.model import Model
 class LeNet5_SS(Model, nn.Module):
     def __init__(self, num_ss_classes=4):
         nn.Module.__init__(self)
-        Model.__init__(self, num_ss_classes, "lenet5", "u7122029/pytorch-cifar10", "lenet5")
+        Model.__init__(self, num_ss_classes, "lenet5", "u7122029/PyTorch_CIFAR10", "lenet5", force_reload=True)
 
         # feature extraction backbone
         self.feat1 = nn.Sequential(self.model.layer1, self.model.layer2)

code/models/linear.py
@@ -6,7 +6,7 @@ from models.model import Model
 class Linear_SS(Model, nn.Module):
     def __init__(self, num_ss_classes=4):
         nn.Module.__init__(self)
-        Model.__init__(self, num_ss_classes, "linear", "u7122029/pytorch-cifar10", "linear", force_reload=False)
+        Model.__init__(self, num_ss_classes, "linear", "u7122029/pytorch_CIFAR10", "linear", force_reload=True)
 
         # The linear model does not have a feature extraction backbone.
 
@@ -21,3 +21,7 @@ class Linear_SS(Model, nn.Module):
         out_class = self.fc(x)
         out_ss = self.fc_ss(x)
         return out_class, out_ss
+
+if __name__ == "__main__":
+    mdl = Linear_SS()
+    print(mdl)

code/utils.py
@@ -281,12 +281,16 @@ def fit_lr(train_x,
 
     plt.figure()
     plt.title(title)
+    plt.grid()
     plt.xlabel(f"{x_task.capitalize()} Accuracy")
     plt.ylabel(f"{y_task.capitalize()} Accuracy")
-    plt.scatter(train_x.reshape(-1, 1), train_y, marker="+", linewidths=0.75, color="blue")
-    plt.scatter(val_x.reshape(-1, 1), val_y, marker="x", linewidths=0.5, color="red")
-    plt.plot(all_x.reshape(-1, 1), all_pred_y_train, "b", label="Interior Domain")
-    plt.plot(all_x.reshape(-1, 1), all_pred_y_val, "r", label="Exterior Domain")
+    plt.scatter(val_x.reshape(-1, 1), val_y, marker="x", alpha=0.5, linewidths=0.5, color="#f08080",
+                label=r"$\bf{ExD}$" + " Datasets")
+    plt.scatter(train_x.reshape(-1, 1), train_y, marker="+", alpha=0.5, linewidths=0.75, color="#A7C7E7",
+                label=r"$\bf{InD}$" + " Datasets")
+    plt.plot(all_x.reshape(-1, 1), all_pred_y_train, "b", label=r"$\bf{In}$" + "terior " + r"$\bf{D}$" + "omain Fit")
+    plt.plot(all_x.reshape(-1, 1), all_pred_y_val, "r", label=r"$\bf{Ex}$" + "terior " + r"$\bf{D}$" + "omain Fit")
+
     plt.legend(loc="best")
     if output:
         p = results_root / output
@@ -333,15 +337,17 @@ def dataset_recurse(data_root: Path, temp_root: Path, name: str, model: Model, p
         return
 
     # Visit all subdirs
-    print(f"Current data collection: {str(data_root)}\tCurrent temp path: {str(temp_root)}")
+    #print(f"Current data collection: {str(data_root)}\tCurrent temp path: {str(temp_root)}")
     out = []
     dirs = sorted(data_root.iterdir())  # Sorting ensures order.
-    for path in tqdm(dirs, total=len(list(dirs))):
+    progressbar = tqdm(dirs, total=len(list(dirs)))
+    for path in progressbar:
+        progressbar.set_postfix({"data_dir": str(data_root)})
         if not path.is_dir():
             # Skip files.
             continue
         entity = path.parts[-1]
-        dataset_recurse(data_root / entity, temp_root / entity, name, model, predictor_func)
+        dataset_recurse(data_root / entity, temp_root / entity, name, model, predictor_func, device, recalculate)
         loaded = np.load(str(temp_root / entity / f"{model.model_name}.npz"))[name]
         out.append(loaded)
     out = np.concatenate(out)